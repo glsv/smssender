38f7726698e2e8344c453e3e36a9634edcdc43f5
glsv <glsv@yandex.ru>
2020-01-26 16:40:08 +0300

    add bootstrap to composer.json

---
+++ src/Bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace glsv\smssender;
+
+use yii\base\BootstrapInterface;
+
+class Bootstrap implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        
+    }
+}
