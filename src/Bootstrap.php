4b68e9b17b852a1519f21dbb9b7a17c4f36fe874
glsv <glsv@yandex.ru>
2020-01-26 16:53:43 +0300

    add i18 config to Bootstrap

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -8,6 +8,13 @@ class Bootstrap implements BootstrapInterface
 {
     public function bootstrap($app)
     {
-        
+        $app->i18n->translations['sms-sender*'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'basePath' => '@vendor/glsv/yii2-smssender/src/messages',
+            'fileMap' => [
+                'sms-sender/app' => 'app.php',
+                'sms-sender/statuses' => 'statuses.php',
+            ]
+        ];
     }
 }
